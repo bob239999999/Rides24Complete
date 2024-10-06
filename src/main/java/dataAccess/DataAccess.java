9fd9bec940b0c92fe3bb3fa6b178ad88f0b1c5d8
maximilianoaraujogalan <maximilianoaraujogalan@192.168.1.19>
2024-10-06 17:45:08 +0200

    Solved HIGH adaptability issue

--- src/main/java/dataAccess/DataAccess.java
+++ src/main/java/dataAccess/DataAccess.java
@@ -32,6 +32,7 @@ public class DataAccess {
 	ConfigXML c = ConfigXML.getInstance();
 	
 	private String adminPass="admin";
+	private static final String DESTINATION_MADRID = "Madrid";
 
 	public DataAccess() {
 		if (c.isDatabaseInitialized()) {
@@ -106,10 +107,10 @@ public class DataAccess {
 			cal.set(2024, Calendar.APRIL, 20);
 			Date date4 = UtilDate.trim(cal.getTime());
 
-			driver1.addRide("Donostia", "Madrid", date2, 5, 20); //ride1
+			driver1.addRide("Donostia", DESTINATION_MADRID, date2, 5, 20); //ride1
 			driver1.addRide("Irun", "Donostia", date2, 5, 2); //ride2
-			driver1.addRide("Madrid", "Donostia", date3, 5, 5); //ride3
-			driver1.addRide("Barcelona", "Madrid", date4, 0, 10); //ride4
+			driver1.addRide(DESTINATION_MADRID, "Donostia", date3, 5, 5); //ride3
+			driver1.addRide("Barcelona", DESTINATION_MADRID, date4, 0, 10); //ride4
 			driver2.addRide("Donostia", "Hondarribi", date1, 5, 3); //ride5
 
 			Ride ride1 = driver1.getCreatedRides().get(0);
