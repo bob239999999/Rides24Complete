396c95daf6e716eabe571d9452603f349eb98c45
maximilianoaraujogalan <maximilianoaraujogalan@192.168.1.19>
2024-10-06 17:36:37 +0200

    Solved low intentionally issue by removing empty declaration

--- src/test/java/CreateRideBDBlackTest.java
+++ src/test/java/CreateRideBDBlackTest.java
@@ -165,7 +165,7 @@ public class CreateRideBDBlackTest {
 		String rideTo=null;
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Date rideDate=null;;
+		Date rideDate=null;
 		try {
 			rideDate = sdf.parse("05/10/2026");
 		} catch (ParseException e) {
